954bd3ce8d78053dd1e663275d8d7eef4a80a24b
Wal M <Ethan_w21@yahoo.com>
2013-11-14 14:01:12 -0600

    created contact page fully works, created Applicant table in database for job applicants

---
+++ 6K185_DayCare_NEW/Msge_confirm.aspx.vb
@@ -0,0 +1,7 @@
+﻿
+Partial Class Msge_confirm
+    Inherits System.Web.UI.Page
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Response.AddHeader("refresh", "2;url=Default.aspx")
+    End Sub
+End Class
