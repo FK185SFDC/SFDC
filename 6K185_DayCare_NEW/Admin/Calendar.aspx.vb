73764c8f647e6f96eb07cfd2118e8ccbe632fefa
Chris Cotton <chris-cotton@uiowa.edu>
2013-11-21 12:53:59 -0600

    modified redirects for calendar buttons

--- 6K185_DayCare_NEW/Admin/Calendar.aspx.vb
+++ 6K185_DayCare_NEW/Admin/Calendar.aspx.vb
@@ -2,9 +2,9 @@
 Partial Class Admin_Calendar
     Inherits System.Web.UI.Page
     Protected Sub createButton_Click(sender As Object, e As EventArgs) Handles createButton.Click
-        Response.Redirect("Create.aspx")
+        Response.Redirect("createEvent.aspx")
     End Sub
     Protected Sub deleteButton_Click(sender As Object, e As EventArgs) Handles deleteButton.Click
-        Response.Redirect("Delete.aspx")
+        Response.Redirect("deleteEvent.aspx")
     End Sub
 End Class
