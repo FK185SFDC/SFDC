eafa5eb89693bab734eedb23df3a1c2b799f7bfc
Chris Cotton <chris-cotton@uiowa.edu>
2013-12-11 18:02:43 -0600

    changed create event page

--- DayCare/Admin/createEvent.aspx
+++ DayCare/Admin/createEvent.aspx
@@ -1,35 +1,165 @@
 ﻿<%@ Page Title="" Language="VB" MasterPageFile="~/MasterPage_Main.master" AutoEventWireup="false" CodeFile="createEvent.aspx.vb" Inherits="Admin_createEvent" %>
 
+
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+      <script type="text/javascript">
+          function PopulateDays() {
+              var ddlMonth = document.getElementById("<%=ddlMonth.ClientID%>");
+            var ddlYear = document.getElementById("<%=ddlYear.ClientID%>");
+            var ddlDay = document.getElementById("<%=ddlDay.ClientID%>");
+            var y = ddlYear.options[ddlYear.selectedIndex].value;
+            var m = ddlMonth.options[ddlMonth.selectedIndex].value != 0;
+            if (ddlMonth.options[ddlMonth.selectedIndex].value != 0 && ddlYear.options[ddlYear.selectedIndex].value != 0) {
+                var dayCount = 32 - new Date(ddlYear.options[ddlYear.selectedIndex].value, ddlMonth.options[ddlMonth.selectedIndex].value - 1, 32).getDate();
+                ddlDay.options.length = 0;
+                AddOption(ddlDay, "DD", "0");
+                for (var i = 1; i <= dayCount; i++) {
+                    AddOption(ddlDay, i, i);
+                }
+            }
+        }
+
+        function AddOption(ddl, text, value) {
+            var opt = document.createElement("OPTION");
+            opt.text = text;
+            opt.value = value;
+            ddl.options.add(opt);
+        }
+
+        function Validate(sender, args) {
+            var ddlMonth = document.getElementById("<%=ddlMonth.ClientID%>");
+    var ddlYear = document.getElementById("<%=ddlYear.ClientID%>");
+    var ddlDay = document.getElementById("<%=ddlDay.ClientID%>");
+    args.IsValid = (ddlDay.selectedIndex != 0 && ddlMonth.selectedIndex != 0 && ddlYear.selectedIndex != 0)
+}
+</script>
+
+     <script type="text/javascript">
+         function PopulateDays2() {
+             var ddlMonth2 = document.getElementById("<%=ddlMonth2.ClientID%>");
+            var ddlYear2 = document.getElementById("<%=ddlYear2.ClientID%>");
+            var ddlDay2 = document.getElementById("<%=ddlDay2.ClientID%>");
+            var y = ddlYear2.options[ddlYear2.selectedIndex].value;
+            var m = ddlMonth2.options[ddlMonth2.selectedIndex].value != 0;
+            if (ddlMonth2.options[ddlMonth2.selectedIndex].value != 0 && ddlYear2.options[ddlYear2.selectedIndex].value != 0) {
+                var dayCount = 32 - new Date(ddlYear2.options[ddlYear2.selectedIndex].value, ddlMonth2.options[ddlMonth2.selectedIndex].value - 1, 32).getDate();
+                ddlDay2.options.length = 0;
+                AddOption2(ddlDay2, "DD", "0");
+                for (var i = 1; i <= dayCount; i++) {
+                    AddOption2(ddlDay2, i, i);
+                }
+            }
+        }
+
+        function AddOption2(ddl, text, value) {
+            var opt = document.createElement("OPTION");
+            opt.text = text;
+            opt.value = value;
+            ddl.options.add(opt);
+        }
+
+        function Validate2(sender, args) {
+            var ddlMonth2 = document.getElementById("<%=ddlMonth2.ClientID%>");
+    var ddlYear2 = document.getElementById("<%=ddlYear2.ClientID%>");
+    var ddlDay2 = document.getElementById("<%=ddlDay2.ClientID%>");
+    args.IsValid = (ddlDay2.selectedIndex != 0 && ddlMonth2.selectedIndex != 0 && ddlYear2.selectedIndex != 0)
+}
+</script>
+
+    
     <h1>Create Event</h1>
 
     <table>
-    <tr>
-        <td><asp:Label ID="locationLabel" runat="server" Text="Location"></asp:Label></td>
-        <td><asp:TextBox ID="whereTextBox" runat="server"></asp:TextBox></td>
-    </tr>
-    <tr>
-     <td><asp:Label ID="startLabel" runat="server" Text="Start Date"></asp:Label></td>
-    <td><asp:TextBox ID="startTextBox" runat="server"></asp:TextBox></td>
-     </tr>
+
         <tr>
-    <td><asp:Label ID="endLabel" runat="server" Text="End Date"></asp:Label></td>
-    <td><asp:TextBox ID="endTextBox" runat="server"></asp:TextBox></td>
-            </tr>
-<tr>
      <td><asp:Label ID="subjectLabel" runat="server" Text="Event Title"></asp:Label></td>
-    <td><asp:TextBox ID="subjectTextBox" runat="server"></asp:TextBox></td>
+    <td><asp:TextBox ID="subjectTextBox" runat="server" ToolTip="Please enter title of event"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="reqTitle" runat="server" controltovalidate="subjectTextBox" 
+            ErrorMessage="Please enter the title of event"></asp:RequiredFieldValidator>
+    </td>
     </tr>
         <tr>
      <td><asp:Label ID="bodyLabel" runat="server" Text="Describe Event"></asp:Label></td>
-    <td><asp:TextBox ID="bodyTextBox" runat="server"></asp:TextBox></td>
+    <td><asp:TextBox ID="bodyTextBox" runat="server" ToolTip="Please enter a brief description of the event"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="reqBody" runat="server" controltovalidate="bodyTextBox" 
+            ErrorMessage="Please enter a brief description of the event"></asp:RequiredFieldValidator>
+    </td>
     </tr>
+
+    <tr>
+        <td><asp:Label ID="locationLabel" runat="server" Text="Address"></asp:Label></td>
+        <td><asp:TextBox ID="whereTextBox" runat="server" ToolTip="Please enter a location for the event."></asp:TextBox>
+            <asp:RequiredFieldValidator ID="reqLoc" runat="server" controltovalidate="whereTextBox" ErrorMessage="Please enter the address of event"></asp:RequiredFieldValidator>
+        </td>
+        
+    </tr>
+    <tr>
+     <td><asp:Label ID="startLabel" runat="server" Text="Start Time"></asp:Label></td>
+    <td>
+         <asp:DropDownList ID="ddlHour" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="ddlMinutes" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="ddlPeriod" runat="server"></asp:DropDownList>
+
+
+    </td>
+        </tr>
+      
+       
+           <tr>
+            <td>Start Date</td>
+            <td>
+               <asp:DropDownList ID="ddlYear" runat="server" onchange = "PopulateDays()"></asp:DropDownList>
+               <asp:DropDownList ID="ddlMonth" runat="server" onchange = "PopulateDays()"></asp:DropDownList>
+               <asp:DropDownList ID="ddlDay" runat="server"></asp:DropDownList>
+                
+                
+                <br /> 
+                <asp:CustomValidator ID="Validator" runat="server" ErrorMessage="* Required"
+                    ClientValidationFunction = "Validate" />
+            </td>
+            
+
+    
+        </tr>
+
+         <tr>
+    <td><asp:Label ID="endLabel" runat="server" Text="End Time"></asp:Label></td>
+    <td>
+         <asp:DropDownList ID="ddlHour2" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="ddlMinutes2" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="ddlPeriod2" runat="server"></asp:DropDownList>
+    </td>     
+            </tr>
+
+        <tr>
+            <td>End Date</td>
+            <td>
+                <asp:DropDownList ID="ddlYear2" runat="server" onchange = "PopulateDays2()"></asp:DropDownList>
+                <asp:DropDownList ID="ddlMonth2" runat="server" onchange = "PopulateDays2()"></asp:DropDownList>
+                <asp:DropDownList ID="ddlDay2" runat="server"></asp:DropDownList>
+                
+                
+                <br /> 
+                <asp:CustomValidator ID="Validator2" runat="server" ErrorMessage="* Required"
+                    ClientValidationFunction = "Validate2" />
+            </td>
+            
+
+    
+        </tr>
+        
+
+     
     </table>
+   
 
 
     <br />
+    
 
     <asp:Label ID="errorLabel" runat="server" Text=""></asp:Label>
 
--- DayCare/Admin/createEvent.aspx.vb
+++ DayCare/Admin/createEvent.aspx.vb
@@ -45,10 +45,40 @@ Partial Class Admin_createEvent
 
         Dim strCalName As String = "Day Care Events"
         Dim strWhere As String = whereTextBox.Text
-        Dim strStart As String = startTextBox.Text
-        Dim strEnd As String = endTextBox.Text
+
+
+        Dim startHour As String = ddlHour.SelectedValue
+        Dim startMinute As String = ddlMinutes.SelectedValue
+        Dim startPeriod As String = ddlPeriod.SelectedValue
+        Dim startTime As String = startHour + ":" + startMinute + " " + startPeriod
+
+        Dim startMonth As String = ddlMonth.SelectedValue
+        Dim startDay As String = ddlDay.SelectedValue
+        Dim startYear As String = ddlYear.SelectedValue
+        Dim strStart As String = startTime + " " + startMonth + "/" + startDay + "/" + startYear
+
+        Dim endHour As String = ddlHour2.SelectedValue
+        Dim endMinute As String = ddlMinutes2.SelectedValue
+        Dim endPeriod As String = ddlPeriod2.SelectedValue
+        Dim endTime As String = endHour + ":" + endMinute + " " + endPeriod
+
+        Dim endMonth As String = ddlMonth2.SelectedValue
+        Dim endDay As String = ddlDay2.SelectedValue
+        Dim endYear As String = ddlYear2.SelectedValue
+        Dim strEnd As String = endTime + " " + endMonth + "/" + endDay + "/" + endYear
+
+
+        'Dim strStartTime As String = startTextBox.Text
+        'Dim strStartDate As String = Calendar1.SelectedDate
+        'Dim strStart As String = strStartTime + " " + strStartDate
+
+        'Dim strEndTime As String = endTextBox.Text
+        'Dim strEndDate As String = Calendar2.SelectedDate
+        'Dim strEnd As String = strEndTime + " " + strEndDate
+
         Dim strSubject As String = subjectTextBox.Text
         Dim strBody As String = bodyTextBox.Text
+
 
         Dim entry As New Google.GData.Calendar.EventEntry
 
@@ -89,6 +119,292 @@ Partial Class Admin_createEvent
         Response.Redirect("Calendar.aspx")
 
     End Sub
+    Private Property Day() As Integer
+        Get
+            If (Not (Request.Form(ddlDay.UniqueID)) Is Nothing) Then
+                Return Integer.Parse(Request.Form(ddlDay.UniqueID))
+            Else
+                Return Integer.Parse(ddlDay.SelectedItem.Value)
+            End If
+        End Get
+        Set(ByVal value As Integer)
+            Me.PopulateDay()
+            ddlDay.ClearSelection()
+            ddlDay.Items.FindByValue(value.ToString).Selected = True
+        End Set
+    End Property
+
+    Private Property Month() As Integer
+        Get
+            Return Integer.Parse(ddlMonth.SelectedItem.Value)
+        End Get
+        Set(ByVal value As Integer)
+            Me.PopulateMonth()
+            ddlMonth.ClearSelection()
+            ddlMonth.Items.FindByValue(value.ToString).Selected = True
+        End Set
+    End Property
+
+    Private Property Year() As Integer
+        Get
+            Return Integer.Parse(ddlYear.SelectedItem.Value)
+        End Get
+        Set(ByVal value As Integer)
+            Me.PopulateYear()
+            ddlYear.ClearSelection()
+            ddlYear.Items.FindByValue(value.ToString).Selected = True
+        End Set
+    End Property
+
+    Public Property SelectedDate() As DateTime
+        Get
+            Try
+                Return DateTime.Parse(Me.Month & "/" & Me.Day & "/" & Me.Year)
+            Catch ex As Exception
+                Return DateTime.MinValue
+            End Try
+        End Get
+        Set(ByVal value As DateTime)
+            If Not value.Equals(DateTime.MinValue) Then
+                Me.Year = value.Year
+                Me.Month = value.Month
+                Me.Day = value.Day
+            End If
+        End Set
+    End Property
+
+    Private Sub PopulateDay()
+        ddlDay.Items.Clear()
+        Dim lt As ListItem = New ListItem
+        lt.Text = "DD"
+        lt.Value = "0"
+        ddlDay.Items.Add(lt)
+        Dim days As Integer = DateTime.DaysInMonth(Me.Year, Me.Month)
+        Dim i As Integer = 1
+        Do While (i <= days)
+            lt = New ListItem
+            lt.Text = i.ToString
+            lt.Value = i.ToString
+            ddlDay.Items.Add(lt)
+            i = (i + 1)
+        Loop
+        ddlDay.Items.FindByValue(DateTime.Now.Day.ToString).Selected = True
+    End Sub
+
+    Private Sub PopulateMonth()
+        ddlMonth.Items.Clear()
+        Dim lt As ListItem = New ListItem
+        lt.Text = "MM"
+        lt.Value = "0"
+        ddlMonth.Items.Add(lt)
+        Dim i As Integer = 1
+        Do While (i <= 12)
+            lt = New ListItem
+            lt.Text = Convert.ToDateTime((i.ToString + "/1/1900")).ToString("MMMM")
+            lt.Value = i.ToString
+            ddlMonth.Items.Add(lt)
+            i = (i + 1)
+        Loop
+        ddlMonth.Items.FindByValue(DateTime.Now.Month.ToString).Selected = True
+    End Sub
+
+    Private Sub PopulateYear()
+        ddlYear.Items.Clear()
+        Dim lt As ListItem = New ListItem
+        lt.Text = "YYYY"
+        lt.Value = "0"
+        ddlYear.Items.Add(lt)
+        Dim i As Integer = DateTime.Now.Year
+        Do While (i >= 1950)
+            lt = New ListItem
+            lt.Text = i.ToString
+            lt.Value = i.ToString
+            ddlYear.Items.Add(lt)
+            i = (i - 1)
+        Loop
+        ddlYear.Items.FindByValue(DateTime.Now.Year.ToString).Selected = True
+    End Sub
+
+    Private Property Day2() As Integer
+        Get
+            If (Not (Request.Form(ddlDay2.UniqueID)) Is Nothing) Then
+                Return Integer.Parse(Request.Form(ddlDay2.UniqueID))
+            Else
+                Return Integer.Parse(ddlDay2.SelectedItem.Value)
+            End If
+        End Get
+        Set(ByVal value As Integer)
+            Me.PopulateDay2()
+            ddlDay2.ClearSelection()
+            ddlDay2.Items.FindByValue(value.ToString).Selected = True
+        End Set
+    End Property
+
+    Private Property Month2() As Integer
+        Get
+            Return Integer.Parse(ddlMonth2.SelectedItem.Value)
+        End Get
+        Set(ByVal value As Integer)
+            Me.PopulateMonth2()
+            ddlMonth2.ClearSelection()
+            ddlMonth2.Items.FindByValue(value.ToString).Selected = True
+        End Set
+    End Property
+
+    Private Property Year2() As Integer
+        Get
+            Return Integer.Parse(ddlYear2.SelectedItem.Value)
+        End Get
+        Set(ByVal value As Integer)
+            Me.PopulateYear2()
+            ddlYear2.ClearSelection()
+            ddlYear2.Items.FindByValue(value.ToString).Selected = True
+        End Set
+    End Property
+
+    Public Property SelectedDate2() As DateTime
+        Get
+            Try
+                Return DateTime.Parse(Me.Month2 & "/" & Me.Day2 & "/" & Me.Year2)
+            Catch ex As Exception
+                Return DateTime.MinValue
+            End Try
+        End Get
+        Set(ByVal value As DateTime)
+            If Not value.Equals(DateTime.MinValue) Then
+                Me.Year2 = value.Year
+                Me.Month2 = value.Month
+                Me.Day2 = value.Day
+            End If
+        End Set
+    End Property
+
+    Private Sub PopulateDay2()
+        ddlDay2.Items.Clear()
+        Dim lt As ListItem = New ListItem
+        lt.Text = "DD"
+        lt.Value = "0"
+        ddlDay2.Items.Add(lt)
+        Dim days As Integer = DateTime.DaysInMonth(Me.Year2, Me.Month2)
+        Dim i As Integer = 1
+        Do While (i <= days)
+            lt = New ListItem
+            lt.Text = i.ToString
+            lt.Value = i.ToString
+            ddlDay2.Items.Add(lt)
+            i = (i + 1)
+        Loop
+        ddlDay2.Items.FindByValue(DateTime.Now.Day.ToString).Selected = True
+    End Sub
+
+    Private Sub PopulateMonth2()
+        ddlMonth2.Items.Clear()
+        Dim lt As ListItem = New ListItem
+        lt.Text = "MM"
+        lt.Value = "0"
+        ddlMonth2.Items.Add(lt)
+        Dim i As Integer = 1
+        Do While (i <= 12)
+            lt = New ListItem
+            lt.Text = Convert.ToDateTime((i.ToString + "/1/1900")).ToString("MMMM")
+            lt.Value = i.ToString
+            ddlMonth2.Items.Add(lt)
+            i = (i + 1)
+        Loop
+        ddlMonth2.Items.FindByValue(DateTime.Now.Month.ToString).Selected = True
+    End Sub
+
+    Private Sub PopulateYear2()
+        ddlYear2.Items.Clear()
+        Dim lt As ListItem = New ListItem
+        lt.Text = "YYYY"
+        lt.Value = "0"
+        ddlYear2.Items.Add(lt)
+        Dim i As Integer = DateTime.Now.Year
+        Do While (i >= 1950)
+            lt = New ListItem
+            lt.Text = i.ToString
+            lt.Value = i.ToString
+            ddlYear2.Items.Add(lt)
+            i = (i - 1)
+        Loop
+        ddlYear2.Items.FindByValue(DateTime.Now.Year.ToString).Selected = True
+    End Sub
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            If (Me.SelectedDate = DateTime.MinValue) Then
+                Me.PopulateYear()
+                Me.PopulateMonth()
+                Me.PopulateDay()
+            End If
+        Else
+            If (Not (Request.Form(ddlDay.UniqueID)) Is Nothing) Then
+                Me.PopulateDay()
+                ddlDay.ClearSelection()
+                ddlDay.Items.FindByValue(Request.Form(ddlDay.UniqueID)).Selected = True
+            End If
+        End If
+
+        If Not IsPostBack Then
+            If (Me.SelectedDate2 = DateTime.MinValue) Then
+                Me.PopulateYear2()
+                Me.PopulateMonth2()
+                Me.PopulateDay2()
+            End If
+        Else
+            If (Not (Request.Form(ddlDay2.UniqueID)) Is Nothing) Then
+                Me.PopulateDay2()
+                ddlDay2.ClearSelection()
+                ddlDay2.Items.FindByValue(Request.Form(ddlDay2.UniqueID)).Selected = True
+            End If
+        End If
+
+
+        Dim h As ListItem = New ListItem
+        Dim m As ListItem = New ListItem
+
+        m.Text = "00"
+        m.Value = "0"
+        ddlMinutes.Items.Add(m)
+        ddlMinutes2.Items.Add(m)
+
+        Dim i As Integer = 1
+        Do While (i <= 12)
+            h = New ListItem
+            h.Text = i.ToString
+            h.Value = i.ToString
+            ddlHour.Items.Add(h)
+            ddlHour2.Items.Add(h)
+            i += 1
+        Loop
+
+        Dim k As Integer = 15
+        Do While (k <= 60)
+            m = New ListItem
+            m.Text = k.ToString
+            m.Value = k.ToString
+            ddlMinutes.Items.Add(m)
+            ddlMinutes2.Items.Add(m)
+            k += 15
+        Loop
+
+        ddlPeriod.Items.Add("AM")
+        ddlPeriod.Items.Add("PM")
+        ddlPeriod2.Items.Add("AM")
+        ddlPeriod2.Items.Add("PM")
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
     Protected Sub cancelButton_Click(sender As Object, e As EventArgs) Handles cancelButton.Click
         Response.Redirect("Calendar.aspx")
     End Sub
